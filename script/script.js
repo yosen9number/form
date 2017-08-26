fc3dee9231995b8dff69ab546f522de683fbba9f
yosen9number <korosu.o.korosu@gmail.com>
2017-08-26 21:48:01 +0300

    modal block
    добавленно модальное окно, кот. будет выводится при успешной регистрации

--- script/script.js
+++ script/script.js
@@ -1,10 +1,12 @@
 var text;
 var value;
 var pattern;
-var info = text + value;
-var elements = document.querySelectorAll('input');
+var nameValue;
+var emailValue;
+var phoneValue;
 
-console.log(elements);
+
+var elements = document.querySelectorAll('input');
 
 
     for (var i = 0; i < elements.length; i++) {
@@ -13,47 +15,56 @@ console.log(elements);
         var name = element.getAttribute('name');
         value = element.value;
 
-        if ( name == 'name') {
+        if ( name === 'name') {
             pattern = /^([a-zA-Zа-яА-ЯёЁ\-]+) ([a-zA-Zа-яА-ЯёЁ\-]+)/;
-            text = 'Имя: ';
-        } else if (type == 'email') {
+            text = 'Имя:';
+        } else if (type === 'email') {
             pattern = /^([a-z0-9_\.-]+)@([a-z0-9_\.-]+)\.([a-z]{2,6})$/;
             text = 'Email: ';
-        } else if (type == 'tel') {
+        } else if (type === 'tel') {
             pattern = /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/;
             text = 'Телефон: ';
         }
 
-        element.addEventListener('change', function () {
-            if (pattern.test(value) == false) {
-                this.classList.add('error');
-                console.log(this.pattern, this.classList, this);
-            }else {
-                this.classList.remove('error');
+        document.querySelector('form').addEventListener('change', function (element, pattern, value) {
+            if (pattern.test(value)) {
+                element.classList.remove('error');
+                return true;
+            } else {
+                element.classList.add('error');
             }
-        })
+        });
+
     }
 
 
+
 function valid (form) {
+    var modal = document.getElementById('modal');
+    var close = modal.querySelector('.close__btn');
+    var options = modal.querySelector('.options');
 
+    options.innerHTML = 
 
-
-
+    close.addEventListener('click', function () {
+        modal.hidden = true;
+    })
 }
 
+
 (function() {
-    var plus = document.querySelector(".add-phone__btn");
+    var plus = document.querySelector('.add-phone__btn');
     var newPhone;
 
-    plus.addEventListener("click", function() {
+    plus.addEventListener('click', function() {
         newPhone = document.createElement('div');
-        newPhone.className = "form-group";
-        newPhone.innerHTML ='<input class=\"form-item\" type=\"tel\">' +
-            '<span class=\"error__text\">* Некорректно введен телефон</span>';
+        newPhone.className = 'form-group';
+        newPhone.innerHTML = '<input class="form-item" type="tel">' +
+            '<span class="error__text">* Некорректно введен телефон</span>';
         plus.parentNode.parentNode.appendChild(newPhone);
     });
 
 })();
+
 
 
